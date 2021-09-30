36812e696233cb3188f1c0805f3b8c0f66cc7024
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-09-29 21:20:40 -0300

    up

--- src/introducao/Aula05EstruturasCondicionais3.java
+++ src/introducao/Aula05EstruturasCondicionais3.java
@@ -1,4 +1,13 @@
 package introducao;
-
+//Operadores ternários
 public class Aula05EstruturasCondicionais3 {
+    public static void main(String[] args) {
+        // se meu salário for > 5000, doar 500 reais pro DevDojo
+       double salario = 6000;
+       String msgDoar = "Vou doar 500 reais pro DevDojo";
+       String msgNDoar = "Não tenho condições de doar, mas terei !";
+
+       //(condição)? verdadeiro : falso
+        System.out.println(salario > 5000 ? msgDoar : msgNDoar);
+    }
 }
