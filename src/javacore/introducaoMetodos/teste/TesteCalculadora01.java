b1fce328e5ea1af2557cbd73381c6cb6c9f12d7f
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-10-04 14:56:15 -0300

    up

--- src/javacore/introducaoMetodos/teste/TesteCalculadora01.java
+++ src/javacore/introducaoMetodos/teste/TesteCalculadora01.java
@@ -1,11 +1,15 @@
 package javacore.introducaoMetodos.teste;
-
+//criação de métodos
 import javacore.introducaoMetodos.dominio.Calculadora;
 
 public class TesteCalculadora01 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        
+
+     // modificador de acesso + Retorna ou não retorna(void) + Parâmetros
+
+     calculadora.somaDoisNumeros();
+     calculadora.subtraiDoisNumeros();
         
     }
 }
