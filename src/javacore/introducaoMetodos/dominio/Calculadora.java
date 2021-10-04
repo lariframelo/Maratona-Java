649204a18b0621c5c66dcfdc7808bdd404154dd1
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-10-04 13:50:21 -0300

    up

---
+++ src/javacore/introducaoMetodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package javacore.introducaoMetodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println(10+10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(20-2);
+    }
+}
---
+++ src/javacore/introducaoMetodos/teste/TesteCalculadora01.java
@@ -0,0 +1,11 @@
+package javacore.introducaoMetodos.teste;
+
+import javacore.introducaoMetodos.dominio.Calculadora;
+
+public class TesteCalculadora01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        
+        
+    }
+}
