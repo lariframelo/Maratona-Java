5c2ae737f61ac6e800d20a23092a9d56c41f8ef8
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-10-05 16:33:57 -0300

    update

--- src/javacore/introducaoMetodos/dominio/Estudante.java
+++ src/javacore/introducaoMetodos/dominio/Estudante.java
@@ -4,4 +4,6 @@ public class Estudante {
     public String nome;
     public int idade;
     public char sexo;
+
+
 }
---
+++ src/javacore/introducaoMetodos/teste/EstudanteTeste01.java
@@ -0,0 +1,9 @@
+package javacore.introducaoMetodos.teste;
+
+import javacore.introducaoMetodos.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+    }
+}
